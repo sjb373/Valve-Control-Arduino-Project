ae278f5dd1804aeaabc3de71d219b886b8e3e94a
Soren Bouma <sjb373@uclive.ac.nz>
2016-04-05 14:12:01 +1200

    Added keyboard control for up to 3 LEDs/mosfets

--- workingLEDserialControl.c
+++ workingLEDserialControl.c
@@ -21,29 +21,54 @@
  (eg to get a delay of 90 seconds, you have to type a9999999999b) but this can be easily 
  fixed with a function in the python interface
  */
-int ledPin=8;
+int aPin=8;
+int bPin=9;
+int cPin=10;
 // int delayTime=0;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(ledPin,OUTPUT);
+  pinMode(aPin,OUTPUT);
+  pinMode(bPin,OUTPUT);
+  pinMode(cPin,OUTPUT);
+
 }
 void loop(){
   if (Serial.available()) {
     int ser=Serial.read();     //read the serial input
     Serial.println("value read from serial:");
     Serial.println(ser);
-    delay(10);
+    delay(5);
     if (ser == 97) {
-      //if user presses key 'a', turn LED on
-      Serial.println("turning LED 1 on");
-      digitalWrite(ledPin,HIGH);
+      //if user presses key 'a', turn LED a on
+      Serial.println("turning LED a (red) on");
+      digitalWrite(aPin,HIGH);
       //delay(3000);
     }
     if (ser == 98) {
-      //if user presses key 'b' turn the LED off
-      Serial.println("turning LED 1 off");
-      digitalWrite(ledPin,LOW);
+      //if user presses key 'b' turn LED b on
+      Serial.println("turning LED b(yellow) on");
+      digitalWrite(bPin,HIGH);
+    }
+     if (ser == 99) {
+      //if user presses key 'c' turn LED c on
+      Serial.println("turning LED c(green) on");
+      digitalWrite(cPin,HIGH);
+    }
+     if (ser == 120) {
+      //if user presses key 'x' turn LED a off
+      Serial.println("turning LED a(Red) off");
+      digitalWrite(aPin,LOW);
+    }
+     if (ser == 121) {
+      //if user presses key 'y' turn LED b off
+      Serial.println("turning LED b(yellow) off");
+      digitalWrite(bPin,LOW);
+    }
+    if (ser == 122) {
+      //if user presses key 'z' turn LED c off
+      Serial.println("turning LED c(green) off");
+      digitalWrite(cPin,LOW);
     }
     if (ser >= 49 and ser < 57) {
       // if user types a number 0-9, delay for that many seconds before executing next command
