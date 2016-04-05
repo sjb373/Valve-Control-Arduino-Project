8ef66f44a7f954d8c3bc9f8cc7847a99d9a1e6ba
Soren Bouma <sjb373@uclive.ac.nz>
2016-04-05 23:20:35 +1200

    Update workingLEDserialControl.c

--- workingLEDserialControl.c
+++ workingLEDserialControl.c
@@ -9,7 +9,7 @@
  >x turns led a off,y turns b off
  s shuts down every led
  o turns on every led
- >typing a number n will cause a delay of n seconds before the next command character is read
+ >typing a number n will cause a delay of n MILISECONDS before the next command character is read
 
  for example a1x1a1x1a1x1a1x1 will cause the led a to flash on and off
 
@@ -38,58 +38,90 @@ void setup(){
   pinMode(aPin,OUTPUT);
   pinMode(bPin,OUTPUT);
   pinMode(cPin,OUTPUT);
+  
 
 }
+
+int aState=0;
+int bState=0;
+int cState=0;
+int displayState = 1;
+
+
 void loop(){
   if (Serial.available()) {
     int ser=Serial.read();     //read the serial input
-    Serial.println("value read from serial:");
-    Serial.println(ser);
-    delay(5);
+    //Serial.println("value read from serial:");
+    //Serial.println(ser);
+    
+    if (displayState == 1){
+     //If a command has just been executed, display current state
+     Serial.println("Current State: (1 on, 0 off)");
+     Serial.printf("Valve 1: %s | Valve 2: %s | Spark Generator: %s",aState,bState,cState);
+     displayState = 0;
+    }
     if (ser == 97) {
       //if user presses key 'a', turn LED a on
       Serial.println("turning LED a (red) on");
       digitalWrite(aPin,HIGH);
+      aState = 1;
+      displayState = 1;
+      
       //delay(3000);
     }
     if (ser == 98) {
       //if user presses key 'b' turn LED b on
       Serial.println("turning LED b(yellow) on");
       digitalWrite(bPin,HIGH);
+      displayState=1;
+      bState=1;
     }
      if (ser == 99) {
       //if user presses key 'c' turn LED c on
       Serial.println("turning LED c(green) on");
       digitalWrite(cPin,HIGH);
+      displayState=1;
+      cState=1
     }
      if (ser == 120) {
       //if user presses key 'x' turn LED a off
       Serial.println("turning LED a(Red) off");
       digitalWrite(aPin,LOW);
+      displayState=1;
+      aState=0;
     }
      if (ser == 121) {
       //if user presses key 'y' turn LED b off
       Serial.println("turning LED b(yellow) off");
       digitalWrite(bPin,LOW);
+      displayState=1;
+      bState=0;
     }
     if (ser == 122) {
       //if user presses key 'z' turn LED c off
       Serial.println("turning LED c(green) off");
       digitalWrite(cPin,LOW);
+      displayState=1;
+      cState=0;
     }
     if (ser >= 49 and ser < 57) {
       // if user types a number 0-9, delay for that many seconds before executing next command
       Serial.println("delaying by this many seconds: ");
       Serial.println(ser-48);
       delay((ser-48)*1000);
+      displayState=1;
     }
      if (ser == 115) {
       // if user types s, turn everything off
       Serial.println("turning everything off");
       Serial.println(ser-48);
       digitalWrite(aPin,LOW);
+      aState=0;
       digitalWrite(bPin,LOW);
+      bState=0;
       digitalWrite(cPin,LOW);
+      cState=0;
+      displayState=1;
 
     }
      if (ser == 111) {
@@ -97,8 +129,12 @@ void loop(){
       Serial.println("turning everything on");
       Serial.println(ser-48);
       digitalWrite(aPin,HIGH);
+      aState=1;
       digitalWrite(bPin,HIGH);
+      bState=1;
       digitalWrite(cPin,HIGH);
+      cState=1;
+      displayState=1;
 
     }
   }
